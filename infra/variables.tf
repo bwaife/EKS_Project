58d463e1498cb8d4fe428f7aab25c70a1c4ec106
Benjamin Waife <bwaife@gmail.com>
2026-05-05 13:50:28 +0100

    changed node size to 3

--- infra/variables.tf
+++ infra/variables.tf
@@ -37,7 +37,7 @@ variable "node_instance_type" {
 variable "node_desired_size" {
   description = "Desired number of worker nodes"
   type        = number
-  default     = 2
+  default     = 3
 }
 
 variable "node_min_size" {
